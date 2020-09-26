3e15f34d8bdff687ce74e6137fb39aa095c79bb6
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-09-26 13:33:26 +0300

    фильтр каталога финиш

--- js/main.js
+++ js/main.js
@@ -29,4 +29,28 @@ $(function () {
 		nextArrow:
 			'<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""/></button>',
 	});
+	$('.filter-style').styler();
+
+	$('.filter__item-drop, .filter__extra').on('click', function () {
+		$(this).toggleClass('filter__item-drop--active');
+		$(this).next().slideToggle('200');
+	});
+
+	$('.js-range-slider').ionRangeSlider({
+		type: 'double',
+		min: 100000,
+		max: 5000000,
+	});
+
+	$('.catalog__filter-btngrid').on('click', function () {
+		$(this).addClass('catalog__filter-button--active');
+		$('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+		$('.product-item__wrapper').removeClass('product-item__wrapper--list');
+	});
+
+	$('.catalog__filter-btnline').on('click', function () {
+		$(this).addClass('catalog__filter-button--active');
+		$('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+		$('.product-item__wrapper').addClass('product-item__wrapper--list');
+	});
 });
